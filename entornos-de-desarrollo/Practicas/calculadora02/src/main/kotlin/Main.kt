11dd590e9636439ef019116d6ac168e8e2cd84d0
Marina Laguna Valdepeñas <123579383+demon-for-arcangel@users.noreply.github.com>
2023-11-01 21:54:35 +0100

    update

---
+++ entornos-de-desarrollo/Practicas/calculadora02/src/main/kotlin/Main.kt
@@ -0,0 +1,77 @@
+/**
+ * Main
+ * programa que simula una calculadora
+ * @param args p
+ */
+fun main(args: Array<String>) {
+
+    var continuar = true
+    while (continuar){
+        println("Ingrese la operación que desea realizar.")
+        println("Suma, resta, multiplicación o división.")
+        val operacion: String = readln()
+        if (operacion != null){
+            /**
+             * Tenemos varias opciones que son los case (suma, resta, multiplicacion, division)
+             * según la que elija el usuario se irá por una opción u otra
+             * Cada opción realiza una operacion distinta.
+             *
+             */
+            when (operacion){
+                "suma" -> {
+                    val num1 = solicitarNum()
+                    val num2 = solicitarNum()
+                    println("La suma de $num1 y $num2 es: ${num1 + num2}")
+                }
+                "resta" -> {
+                    val num1 = solicitarNum()
+                    val num2 = solicitarNum()
+                    println("La resta de $num1 y $num2 es: ${num1 - num2}")
+                }
+                "multiplicacion" -> {
+                    val num1 = solicitarNum()
+                    val num2 = solicitarNum()
+                    println("La multiplicación de $num1 y $num2 es: ${num1 * num2}")
+                }
+                "división" -> {
+                    val num1 = solicitarNum()
+                    val num2 = solicitarNum()
+                    println("la división de $num1 y $num2 es: ${num1 / num2}")
+                } else -> {
+                println("No es posible dividir entre cero.")
+            }
+            }
+        }else{
+            println("Operación no válida, por favor introduzca de nuevo.")
+        }
+        continuar = solicitarContinuar()
+    }
+}
+
+
+/**
+ * Solicitar num
+ *
+ * @return
+ */
+fun solicitarNum(): Double{
+    print("Ingrese un número: ")
+    val numero = readln()
+    return if (numero != null){
+        numero.toDouble()
+    } else {
+        0.0
+    }
+}
+
+
+/**
+ * Solicitar continuar
+ *
+ * @return
+ */
+fun solicitarContinuar(): Boolean{
+    print("¿Desea continuar? (s/n)")
+    val respuesta = readln()
+    return respuesta == "s"
+}
